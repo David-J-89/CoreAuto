bb3d83da81d3ed8a180d544c99c52a9dce29006d
David Whaley <davidwhaleycode@outlook.com>
2019-03-10 19:01:19 -0500

    domain init

---
+++ coreauto/Models/Make.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace coreauto.Models
+{
+    public class Make
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Model> Models { get; set; }
+
+        //iniatlize the collection with a constructor
+        public Make()
+        {
+            Models = new Collection<Model>();
+        }
+    }
+}
---
+++ coreauto/Models/Model.cs
@@ -0,0 +1,10 @@
+﻿namespace coreauto.Models
+{
+    public class Model
+    {
+        public int Int { get; set; }
+        public int Name { get; set; }
+        public Make Make { get; set; } //navigation property to make sure both make and model point back to each other. 
+        public int MakeId { get; set; } //FK property association.
+    }
+}
